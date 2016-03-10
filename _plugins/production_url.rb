e758961dea935375c6c229071f46ab2ea6fd2a86
Chris Mytton <chrismytton@gmail.com>
2016-03-10 13:30:04 +0100

    Hard-code http in production url plugin for now
    It seems that the Facebook comments plugin doesn't work if the url
    doesn't have a protocol.

--- _plugins/production_url.rb
+++ _plugins/production_url.rb
@@ -1,7 +1,7 @@
 class ProductionUrl < Jekyll::Generator
   def generate(site)
     if Jekyll.env == 'production'
-      site.config['url'] = "//#{File.read('CNAME').chomp}"
+      site.config['url'] = "http://#{File.read('CNAME').chomp}"
     end
   end
 end
